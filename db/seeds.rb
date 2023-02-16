4644d212effd1a46839776a763b1c83909b07649
Naomi Christie <naomi.christie@gmail.com>
2023-02-16 11:50:16 +0000

    Seed development db with some values
    I chose 'how', 'who' and 'milk' as first three entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Lookup.create!(word: 'milk', video_url: ['https://youtu.be/DSVMzSZdweY?t=9'])
+Lookup.create!(word: 'how', video_url: ['https://youtu.be/zftO6LJyTXI?t=24'])
+Lookup.create!(word: 'who', video_url: ['https://youtu.be/zftO6LJyTXI?t=121'])
+Lookup.create!(word: 'brush_teeth', video_url: ['https://youtu.be/kKjlfccBrxs?t=19'])
+Lookup.create!(word: 'hello', video_url: ['https://youtu.be/DClaou4oR6c?t=3'])
+
+
+
+
