3a86195e1dcb5a27d861a614ce070b2228561f5d
Naomi Christie <naomi.christie@fundingcircle.com>
2023-02-17 12:04:58 +0000

    Show multiple videos
    Uses chat GPT again to make this happen.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,12 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Lookup.create!(word: 'milk', video_url: ['https://youtube.com/embed/DSVMzSZdweY?start=9&autoplay=1'])
-Lookup.create!(word: 'how', video_url: ['https://youtube.com/embed/zftO6LJyTXI?start=24&autoplay=1'])
-Lookup.create!(word: 'who', video_url: ['https://youtube.com/embed/zftO6LJyTXI?start=121&autoplay=1'])
-Lookup.create!(word: 'brush_teeth', video_url: ['https://youtube.com/embed/kKjlfccBrxs?start=19&autoplay=1'])
-Lookup.create!(word: 'hello', video_url: ['https://youtube.com/embed/DClaou4oR6c?start=3&autoplay=1'])
-Lookup.create!(word: 'strawberry', video_url: ['https://www.youtube.com/embed/89CUYNxZioM?start=9&autoplay=1'])
+# Lookup.create!(word: 'milk', video_url: ['https://youtube.com/embed/DSVMzSZdweY?start=9&autoplay=1'])
+# Lookup.create!(word: 'how', video_url: ['https://youtube.com/embed/zftO6LJyTXI?start=24&autoplay=1'])
+# Lookup.create!(word: 'who', video_url: ['https://youtube.com/embed/zftO6LJyTXI?start=121&autoplay=1'])
+# Lookup.create!(word: 'brush_teeth', video_url: ['https://youtube.com/embed/kKjlfccBrxs?start=19&autoplay=1'])
+# Lookup.create!(word: 'hello', video_url: ['https://youtube.com/embed/DClaou4oR6c?start=3&autoplay=1'])
+# Lookup.create!(word: 'strawberry', video_url: ['https://www.youtube.com/embed/89CUYNxZioM?start=9&autoplay=1'])
+
+Lookup.create!(word: 'happy', video_url: ['https://youtube.com/embed/EfrJriE5Hwg?start=80&autoplay=1', 'https://youtube.com/embed/JoJC0TSFb5I?start=7&autoplay=1'])
+
